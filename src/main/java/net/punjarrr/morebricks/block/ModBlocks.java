d3bbe8cbd16b9b86aea8953c56ae90cfcca550da
Punnathorn Vettayanont <punnathorn.vet@gmail.com>
2023-10-10 13:33:50 +0700

    Update

--- src/main/java/net/punjarrr/morebricks/block/ModBlocks.java
+++ src/main/java/net/punjarrr/morebricks/block/ModBlocks.java
@@ -1,10 +1,7 @@
 package net.punjarrr.morebricks.block;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.MapColor;
+import net.minecraft.block.*;
 import net.minecraft.block.enums.Instrument;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -14,26 +11,40 @@ import net.minecraft.util.Identifier;
 import net.punjarrr.morebricks.MoreBricks;
 
 public class ModBlocks {
-    public static final Block GRANITE_BRICKS = ModBlocks.registerBlock("granite_bricks",
+    public static final Block GRANITE_BRICKS = ModBlocks.register("granite_bricks",
             new Block(AbstractBlock.Settings.create()
                     .mapColor(MapColor.DIRT_BROWN)
                     .instrument(Instrument.BASEDRUM)
                     .requiresTool()
                     .strength(1.5f, 6.0f)));
-    public static final Block DIORITE_BRICKS = ModBlocks.registerBlock("diorite_bricks",
+    public static final Block DIORITE_BRICKS = ModBlocks.register("diorite_bricks",
             new Block(AbstractBlock.Settings.create()
                     .mapColor(MapColor.OFF_WHITE)
                     .instrument(Instrument.BASEDRUM)
                     .requiresTool()
                     .strength(1.5f, 6.0f)));
-    public static final Block ANDESITE_BRICKS = ModBlocks.registerBlock("andesite_bricks",
+    public static final Block ANDESITE_BRICKS = ModBlocks.register("andesite_bricks",
             new Block(AbstractBlock.Settings.create()
                     .mapColor(MapColor.STONE_GRAY)
                     .instrument(Instrument.BASEDRUM)
                     .requiresTool()
                     .strength(1.5f, 6.0f)));
 
-    private static Block registerBlock(String name, Block block) {
+    public static final Block GRANITE_BRICK_STAIRS = ModBlocks.register("granite_brick_stairs",
+            (Block)new StairsBlock(GRANITE_BRICKS.getDefaultState(), AbstractBlock.Settings.copy(GRANITE_BRICKS)));
+    public static final Block DIORITE_BRICK_STAIRS = ModBlocks.register("diorite_brick_stairs",
+            (Block)new StairsBlock(DIORITE_BRICKS.getDefaultState(), AbstractBlock.Settings.copy(DIORITE_BRICKS)));
+    public static final Block ANDESITE_BRICK_STAIRS = ModBlocks.register("andesite_brick_stairs",
+            (Block)new StairsBlock(ANDESITE_BRICKS.getDefaultState(), AbstractBlock.Settings.copy(ANDESITE_BRICKS)));
+
+    public static final Block GRANITE_BRICK_SLAB = ModBlocks.register("granite_brick_slab",
+            (Block)new SlabBlock(AbstractBlock.Settings.copy(GRANITE_BRICKS)));
+    public static final Block DIORITE_BRICK_SLAB = ModBlocks.register("diorite_brick_slab",
+            (Block)new SlabBlock(AbstractBlock.Settings.copy(DIORITE_BRICKS)));
+    public static final Block ANDESITE_BRICK_SLAB = ModBlocks.register("andesite_brick_slab",
+            (Block)new SlabBlock(AbstractBlock.Settings.copy(ANDESITE_BRICKS)));
+
+    private static Block register(String name, Block block) {
         registerBlockItem(name, block);
         return Registry.register(Registries.BLOCK, new Identifier(MoreBricks.MOD_ID, name), block);
     }
