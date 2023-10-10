a41c1c7d3237c3899810f970a623c46a35142b84
Punnathorn Vettayanont <punnathorn.vet@gmail.com>
2023-10-10 15:47:29 +0700

    Add folder to .gitignore

--- src/main/java/net/punjarrr/morebricks/block/ModBlocks.java
+++ src/main/java/net/punjarrr/morebricks/block/ModBlocks.java
@@ -1,16 +1,11 @@
 package net.punjarrr.morebricks.block;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.itemgroup.v1.*;
 import net.minecraft.block.*;
 import net.minecraft.block.enums.Instrument;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.item.Items;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
+import net.minecraft.item.*;
+import net.minecraft.registry.*;
 import net.minecraft.util.Identifier;
 import net.punjarrr.morebricks.MoreBricks;
 
